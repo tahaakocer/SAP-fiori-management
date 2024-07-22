7a05a646316f8a4b71cc463f841bf693f9fdc100
tahaakocer <taha.kocer317@gmail.com>
2024-07-22 15:47:31 +0300

    eski servisten önceki son hal

--- webapp/controller/AddProduct.controller.js
+++ webapp/controller/AddProduct.controller.js
@@ -6,5 +6,46 @@ sap.ui.define([
     "use strict";
 
     return Controller.extend("com.solvia.management.controller.AddProduct", {
+        /**
+         * @override
+         */
+        onInit: function () {
+            this._wizard = this.byId("idCreateProductWizard");
+            this._oNavContainer = this.byId("idWizardNavContainer");
+            this._oWizardContentPage = this.byId("idWizardContentPage");
+
+
+        },
+
+        onWizardComplete: function (oEvent) {
+            this._oNavContainer.to(this.byId("idWizardReviewPage"));
+
+        },
+
+        onSubmitButtonPress: function (oEvent) {
+
+        },
+
+        onCancelButtonPress: function (oEvent) {
+
+        },
+
+        onSegmentedButtonSelectionChange: function (oEvent) {
+
+        },
+		onWizardStepActivate: function(oEvent) {
+			
+		},
+
+		onProductNameInputLiveChange: function(oEvent) {
+			
+		},
+
+		onProductWeightInputLiveChange: function(oEvent) {
+			
+		},
+		onImageWizardStepActivate: function(oEvent) {
+			
+		}
     });
 });
