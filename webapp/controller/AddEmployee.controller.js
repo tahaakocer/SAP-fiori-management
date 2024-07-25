d3a388ed7134ec77b7fab797899ecfff4971aaa6
tahaakocer <taha.kocer317@gmail.com>
2024-07-25 11:48:29 +0300

    create employee

--- webapp/controller/AddEmployee.controller.js
+++ webapp/controller/AddEmployee.controller.js
@@ -1,9 +1,13 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"com/solvia/management/utils/Helper"
+	"com/solvia/management/utils/Helper",
+	"sap/m/MessageToast",
+	"sap/m/MessageBox"
 ], function (
 	Controller,
 	Helper,
+	MessageToast,
+	MessageBox,
 
 ) {
 	"use strict";
@@ -65,18 +69,58 @@ sap.ui.define([
 			var sSalary = this.byId("idSalaryInput").getValue();
 			var oFileUploader = this.byId("idFileUploader");
 
-			
-			console.log(this._sFileContent);
+			var oAddEmpData = {
+				Name: sName,
+				Surname: sSurname,
+				Salary: sSalary,
+				Department: sDepartment,
+				PhoneNumber: sNumber,
+				Birthday: sDate,
+				Email: sEmail,
+				Pimage: this._sFileContent
+			}
 
+			if (Helper.validationForm(oAddEmpData)) {
+				oDataModel.create("/employeeSet", oAddEmpData, {
+					method: "POST",
+					success: function (data) {
+						MessageToast.show("Çalışan başarıyla eklendi.");
+					},
+					error: function (data) {
+						MessageToast.show("Çalışan eklenemedi!");
+						console.error("Çalışan eklenemedi!");
+					}
+				});
+			} else {
+				MessageToast.show("Lütfen gerekli alanları doldurunuz");
+			}
 
 		},
+		_clearFormFields: function () {
+			var oView = this.getView();
 
+			oView.byId("idNameInput").setValue("");
+			oView.byId("idSurnameInput").setValue("");
+			oView.byId("idBirthdayDatePicker").setDateValue(null);
+			oView.byId("idNumberInput").setValue("");
+			oView.byId("idEmailInput").setValue("");
+			oView.byId("idDepartmentSegmentedButton").setSelectedKey("");
+			oView.byId("idSalaryInput").setValue("");
+			oView.byId("idFileUploader").setValue(null);
+		},
+		_handleMessageBoxOpen: function (sMessage, sMessageBoxType) {
+			MessageBox[sMessageBoxType](sMessage, {
+				actions: [MessageBox.Action.YES, MessageBox.Action.NO],
+				onClose: function (oAction) {
+					if (oAction === MessageBox.Action.YES) {
+						this._clearFormFields();
+						this._wizard.discardProgress(this.byId("idInfoWizardStep"));
+					}
+				}.bind(this)
+			});
+		},
 		onCancelButtonPress: function (oEvent) {
-
-		},
-
-
-		onWizardStepImageActivate: function (oEvent) {
+			this._handleMessageBoxOpen("Çalışan ekleme işlemini iptal etmek istiyor musunuz?", "warning");
 
 		},
 
